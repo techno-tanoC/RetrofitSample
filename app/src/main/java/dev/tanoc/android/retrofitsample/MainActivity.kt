758365431f00ed00bf1dfa29377e29bc7a9e901f
techno-tanoC <develop.tanoc@gmail.com>
2021-08-21 18:31:42 +0900

    RetrofitSample

--- app/src/main/java/dev/tanoc/android/retrofitsample/MainActivity.kt
+++ app/src/main/java/dev/tanoc/android/retrofitsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.tanoc.android.retrofitsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -8,7 +9,37 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import dev.tanoc.android.retrofitsample.ui.theme.RetrofitSampleTheme
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+data class Album(
+    val id: Int,
+    val userId: Int,
+    val title: String,
+)
+
+interface Api {
+    @GET("/albums")
+    suspend fun index(): Response<List<Album>>
+}
+
+class Repository(url: String) {
+    private var api: Api = Retrofit
+        .Builder()
+        .baseUrl(url)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create(Api::class.java)
+
+    suspend fun index(): Response<List<Album>> {
+        return api.index()
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +51,15 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+        }
+
+        lifecycleScope.launch {
+            Log.i("RetrofitSample", "start")
+            val response = Repository("https://jsonplaceholder.typicode.com/").index()
+            response.body()?.forEach {
+                Log.i("RetrofitSample", it.toString())
+            }
+            Log.i("RetrofitSample", "finish")
         }
     }
 }
